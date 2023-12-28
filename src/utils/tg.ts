59a8c69fde0c1a9788bdbd2719181fb70d296d7d
Alexey Potapov <2furius@gmail.com>
2023-12-28 13:45:43 +0300

    научились подключаться к MTProto серверам.

--- src/utils/tg.ts
+++ src/utils/tg.ts
@@ -2,9 +2,7 @@ import { TelegramClient} from "telegram";
 import { StringSession } from "telegram/sessions";
 
 export async function init_client(apiHash, apiId) {
-    const client = new TelegramClient(new StringSession(""), apiId, apiHash, {connectionRetries: 5});
-    await client.connect(
-        
-    )
-    await client.sendMessage("me", {message: 'Hello'});
+    const client = new TelegramClient(new StringSession(""), parseInt(apiId), apiHash, {connectionRetries: 5, useWSS: true});
+    await client.connect();
+    // await client.sendMessage("me", {message: 'Hello'});
 }
