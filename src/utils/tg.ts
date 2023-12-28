024d4235a843427bd9c64854628be62bdbbb13c8
Alexey Potapov <2furius@gmail.com>
2023-12-28 14:02:33 +0300

    Временный код для получения qr кода

--- src/utils/tg.ts
+++ src/utils/tg.ts
@@ -4,5 +4,24 @@ import { StringSession } from "telegram/sessions";
 export async function init_client(apiHash, apiId) {
     const client = new TelegramClient(new StringSession(""), parseInt(apiId), apiHash, {connectionRetries: 5, useWSS: true});
     await client.connect();
+    const user = await client.signInUserWithQrCode({ apiId, apiHash },
+        {
+              onError: async function(p1: Error) {
+                  console.log("error", p1);
+                  // true = stop the authentication processes
+                  return true;
+              },
+              qrCode: async (code) => {
+                  console.log("Convert the next string to a QR code and scan it");
+                  console.log(
+                      `tg://login?token=${code.token.toString("base64url")}`
+                  );
+              },
+              password: async (hint) => {
+                  // password if needed
+                  return "1111";
+              }
+          }
+        );
     // await client.sendMessage("me", {message: 'Hello'});
 }
