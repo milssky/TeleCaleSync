1eaba417a2dcad8899cc3409b34a3da089330313
Alexey Potapov <2furius@gmail.com>
2024-02-18 14:19:40 +0300

    Format code

--- src/utils/tg.ts
+++ src/utils/tg.ts
@@ -1,120 +1,147 @@
-import { TelegramClient, password} from "telegram";
+import { TelegramClient, password } from "telegram";
 import { StoreSession } from "telegram/sessions";
-import QRCode  from "qrcode";
-
+import QRCode from "qrcode";
 
 const NotConnected = new Error("Not connected to Telegram API");
 
-
-export async function initClient(apiHash, apiId, container: HTMLDivElement, password?: string) {
-    console.log(apiId);
-    const client = new TelegramClient(new StoreSession("store_id"), parseInt(apiId), apiHash, {connectionRetries: 5, useWSS: true});
-    await client.connect();
-    await client.session.load()
-    // await client.sendMessage("me", {message: 'Hello1234' });
-    await client.signInUserWithQrCode({ apiId, apiHash },
-        {
-              onError: async function(p1: Error) {
-                  console.log("error", p1);
-                  // true = stop the authentication processes
-                  return true;
-              },
-              qrCode: async (code) => {
-                  const url = `tg://login?token=${code.token.toString("base64url")}`
-                  console.log("Convert the next string to a QR code and scan it");
-                  console.log(url);
-                  const qrCodeSvg = await QRCode.toString(url, {type: "svg"});
-                  const parser = new DOMParser();
-                  const svg = parser.parseFromString(qrCodeSvg, "image/svg+xml").documentElement;
-                  svg.setAttribute("width", "150");
-                  svg.setAttribute("height", "150");
-                  while (container.firstChild) {
-                      container.removeChild(container.firstChild);
-                  }
-                  container.appendChild(svg);
-              },
-              // eslint-disable-next-line @typescript-eslint/no-unused-vars
-              password: async (hint) => {
-                  // password if needed
-                  return password ? password : "";
-              }
-          }
-        );
-    await client.session.save()
-    await client.sendMessage("me", {message: 'Hello' });
+export async function initClient(
+	apiHash,
+	apiId,
+	container: HTMLDivElement,
+	password?: string
+) {
+	console.log(apiId);
+	const client = new TelegramClient(
+		new StoreSession("store_id"),
+		parseInt(apiId),
+		apiHash,
+		{ connectionRetries: 5, useWSS: true }
+	);
+	await client.connect();
+	await client.session.load();
+	// await client.sendMessage("me", {message: 'Hello1234' });
+	await client.signInUserWithQrCode(
+		{ apiId, apiHash },
+		{
+			onError: async function (p1: Error) {
+				console.log("error", p1);
+				// true = stop the authentication processes
+				return true;
+			},
+			qrCode: async (code) => {
+				const url = `tg://login?token=${code.token.toString(
+					"base64url"
+				)}`;
+				console.log("Convert the next string to a QR code and scan it");
+				console.log(url);
+				const qrCodeSvg = await QRCode.toString(url, { type: "svg" });
+				const parser = new DOMParser();
+				const svg = parser.parseFromString(
+					qrCodeSvg,
+					"image/svg+xml"
+				).documentElement;
+				svg.setAttribute("width", "150");
+				svg.setAttribute("height", "150");
+				while (container.firstChild) {
+					container.removeChild(container.firstChild);
+				}
+				container.appendChild(svg);
+			},
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			password: async (hint) => {
+				// password if needed
+				return password ? password : "";
+			},
+		}
+	);
+	await client.session.save();
+	await client.sendMessage("me", { message: "Hello" });
 }
 
-export async function signInWithQRCode(container: HTMLDivElement, password?: string) {
-    const url = "tg://login?token="
-    const qrCodeSvg = await QRCode.toString(url, {type: "svg"});
-    const parser = new DOMParser();
-    const svg = parser.parseFromString(qrCodeSvg, "image/svg+xml").documentElement;
-    svg.setAttribute("width", "150");
-    svg.setAttribute("height", "150");
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    container.appendChild(svg);
+export async function signInWithQRCode(
+	container: HTMLDivElement,
+	password?: string
+) {
+	const url = "tg://login?token=";
+	const qrCodeSvg = await QRCode.toString(url, { type: "svg" });
+	const parser = new DOMParser();
+	const svg = parser.parseFromString(
+		qrCodeSvg,
+		"image/svg+xml"
+	).documentElement;
+	svg.setAttribute("width", "150");
+	svg.setAttribute("height", "150");
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+	container.appendChild(svg);
 }
 
 export class TgClient {
-    apiHash: string;
-    apiId: number;
-    _client: TelegramClient;
-    _isConfigured = false;
-    
-    configureClient (apiHash, apiId) {
-        this.apiHash = apiHash;
-        this.apiId = apiId;
-        this._client = new TelegramClient(
-            new StoreSession("store_id"),  //TODO изменить на генератор ID
-             parseInt(apiId),
-             apiHash,
-             {connectionRetries: 5, useWSS: true}
+	apiHash: string;
+	apiId: number;
+	_client: TelegramClient;
+	_isConfigured = false;
 
-        )
-        this._isConfigured = true;
-    }
+	configureClient(apiHash, apiId) {
+		this.apiHash = apiHash;
+		this.apiId = apiId;
+		this._client = new TelegramClient(
+			new StoreSession("store_id"), //TODO изменить на генератор ID
+			parseInt(apiId),
+			apiHash,
+			{ connectionRetries: 5, useWSS: true }
+		);
+		this._isConfigured = true;
+	}
 
-    async connect() {
-        // if (!this._isConfigured && !this._client.connected) {
-        //     throw NotConnected;
-        // }
-        await this._client.connect();
-    }       
+	async connect() {
+		// if (!this._isConfigured && !this._client.connected) {
+		//     throw NotConnected;
+		// }
+		await this._client.connect();
+	}
 
-    async loginWithQRCode(container: HTMLDivElement, password?: string) {
-        if (!this._client.connected) {
-            await this.connect();
-        }
-        await this._client.session.load();
-        await this._client.signInUserWithQrCode({ apiId: this.apiId, apiHash:this.apiHash },
-            {
-                  onError: async function(p1: Error) {
-                      console.log("error", p1);
-                      // true = stop the authentication processes
-                      return true;
-                  },
-                  qrCode: async (code) => {
-                      const url = `tg://login?token=${code.token.toString("base64url")}`
-                      console.log("Convert the next string to a QR code and scan it");
-                      console.log(url);
-                      const qrCodeSvg = await QRCode.toString(url, {type: "svg"});
-                      const parser = new DOMParser();
-                      const svg = parser.parseFromString(qrCodeSvg, "image/svg+xml").documentElement;
-                      svg.setAttribute("width", "150");
-                      svg.setAttribute("height", "150");
-                      while (container.firstChild) {
-                          container.removeChild(container.firstChild);
-                      }
-                      container.appendChild(svg);
-                  },
-                  password: async (hint) => {
-                      return password ? password : "";
-                  }
-              }
-            );
-
-    }
-
+	async loginWithQRCode(container: HTMLDivElement, password?: string) {
+		if (!this._client.connected) {
+			await this.connect();
+		}
+		await this._client.session.load();
+		await this._client.signInUserWithQrCode(
+			{ apiId: this.apiId, apiHash: this.apiHash },
+			{
+				onError: async function (p1: Error) {
+					console.log("error", p1);
+					// true = stop the authentication processes
+					return true;
+				},
+				qrCode: async (code) => {
+					const url = `tg://login?token=${code.token.toString(
+						"base64url"
+					)}`;
+					console.log(
+						"Convert the next string to a QR code and scan it"
+					);
+					console.log(url);
+					const qrCodeSvg = await QRCode.toString(url, {
+						type: "svg",
+					});
+					const parser = new DOMParser();
+					const svg = parser.parseFromString(
+						qrCodeSvg,
+						"image/svg+xml"
+					).documentElement;
+					svg.setAttribute("width", "150");
+					svg.setAttribute("height", "150");
+					while (container.firstChild) {
+						container.removeChild(container.firstChild);
+					}
+					container.appendChild(svg);
+				},
+				password: async (hint) => {
+					return password ? password : "";
+				},
+			}
+		);
+	}
 }
